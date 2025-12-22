1a7b8e0dea282e4ac568211500ebcd5a024e9382
Loïc Souêtre <loic.soutre@sfr.fr>
2025-12-22 16:38:47 +0100

    feat(project): initialize project with Nuxt setup
    * Add package.json with dependencies and scripts
    * Include favicon.ico for branding
    * Create renovate.json for dependency management
    * Add tsconfig.json for TypeScript configuration

---
+++ app/app.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { createClient } from '@supabase/supabase-js'
+
+const config = useRuntimeConfig()
+const supabase = createClient(
+  config.public.supabaseUrl,
+  config.public.supabasePublishableKey
+)
+const instruments = ref([])
+async function getInstruments() {
+  const { data } = await supabase.from('instruments').select()
+  instruments.value = data
+}
+onMounted(() => {
+  getInstruments()
+})
+
+useHead({
+  meta: [{ name: 'viewport', content: 'width=device-width, initial-scale=1' }],
+  link: [{ rel: 'icon', href: '/favicon.ico' }],
+  htmlAttrs: {
+    lang: 'en'
+  }
+})
+
+const title = 'Nuxt Starter Template'
+const description
+  = 'A production-ready starter template powered by Nuxt UI. Build beautiful, accessible, and performant applications in minutes, not hours.'
+
+useSeoMeta({
+  title,
+  description,
+  ogTitle: title,
+  ogDescription: description,
+  ogImage: 'https://ui.nuxt.com/assets/templates/nuxt/starter-light.png',
+  twitterImage: 'https://ui.nuxt.com/assets/templates/nuxt/starter-light.png',
+  twitterCard: 'summary_large_image'
+})
+</script>
+
+<template>
+  <ul>
+    <li
+      v-for="instrument in instruments"
+      :key="instrument.id"
+    >
+      {{ instrument.name }}
+    </li>
+  </ul>
+</template>
