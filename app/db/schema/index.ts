9f8d838589739847bb0e7be94446a1d521f27400
Loïc Souêtre <loic.souetre@sfr.fr>
2025-12-25 22:58:46 +0100

    chore(db): update schema path in drizzle config

--- app/db/schema/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './users'
-export * from './clients'
-export * from './contacts'
-export * from './projects'
-export * from './tasks'
-export * from './relations'
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'drizzle-kit'
 
 export default defineConfig({
   out: './drizzle',
-  schema: './app/db/schema/index.ts',
+  schema: './app/db/schema',
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL!
