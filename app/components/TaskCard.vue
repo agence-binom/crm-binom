50b4ec29b9aa6235b6a52d2a0b2b52cffabbec4e
Loïc Souêtre <loic.souetre@sfr.fr>
2025-12-27 10:54:12 +0100

    feat(task): update layout of task card component
    * Adjust structure for better alignment
    * Maintain functionality for update and delete actions

--- app/components/TaskCard.vue
+++ app/components/TaskCard.vue
@@ -47,40 +47,42 @@ const emit = defineEmits<{
 </script>
 
 <template>
-  <UCard class="shrink-0 hover:shadow-md transition-shadow">
-    <template #header>
-      <div class="flex items-start gap-2">
-        <UIcon
-          name="i-lucide-grip-vertical"
-          class="kanban-handle text-gray-400 hover:text-gray-600 cursor-grab active:cursor-grabbing shrink-0 mt-0.5"
-        />
-        <div class="flex-1 min-w-0">
-          <h4 class="font-semibold text-sm ui-text-highlighted">
-            {{ props.task.title }}
-          </h4>
-          <p class="text-xs ui-text-muted mt-1">
-            {{ props.task.notes }}
-          </p>
+  <div class="flex items-center gap-2 ">
+    <UIcon
+      name="i-lucide-grip-vertical"
+      class="kanban-handle text-gray-400 hover:text-gray-600 cursor-grab active:cursor-grabbing shrink-0 mt-0.5"
+    />
+    <UCard class="shrink-0 flex-1 hover:shadow-md transition-shadow">
+      <template #header>
+        <div class="flex items-start gap-2">
+          <div class="flex-1 min-w-0">
+            <h4 class="font-semibold text-sm ui-text-highlighted">
+              {{ props.task.title }}
+            </h4>
+            <p class="text-xs ui-text-muted mt-1">
+              {{ props.task.notes }}
+            </p>
+          </div>
         </div>
-      </div>
-    </template>
-    <template #footer>
-      <div class="flex justify-end gap-2">
-        <UButton
-          icon="i-lucide-edit"
-          size="xs"
-          color="neutral"
-          variant="ghost"
-          @click="emit('update', props.task.id)"
-        />
-        <UButton
-          icon="i-lucide-trash-2"
-          size="xs"
-          color="error"
-          variant="ghost"
-          @click="emit('delete', props.task.id)"
-        />
-      </div>
-    </template>
-  </UCard>
+      </template>
+      <template #footer>
+        <div class="flex justify-end gap-2">
+          <UButton
+            icon="i-lucide-edit"
+            size="xs"
+            color="neutral"
+            variant="ghost"
+            @click="emit('update', props.task.id)"
+          />
+          <UButton
+            icon="i-lucide-trash-2"
+            size="xs"
+            color="error"
+            variant="ghost"
+            @click="emit('delete', props.task.id)"
+          />
+        </div>
+      </template>
+    </UCard>
+  </div>
 </template>
